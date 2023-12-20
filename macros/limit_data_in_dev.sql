8865e5689148fbad5e97c3f1a958857eae5b82d9
Juan Brekes <jbrekesdata@gmail.com>
2023-12-20 13:18:52 +0000

    Fixes

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,7 +1,7 @@
 {%- macro limit_data_in_dev(column_name, dev_day_of_data=3) -%}
 
 {%- if target.name == 'default' -%}
-where {{ column_name }} >= dateadd('day', - {{ dev_day_of_data }}, current_timestamp)
+where {{ column_name }} >= date_add(current_timestamp, INTERVAL {{ dev_day_of_data }} DAY)
 {%- endif -%}
 
 {%- endmacro -%}
